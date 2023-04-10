746eb0426b86665e35b46beef9d6f1e8f89bfd28
Ermal Bujupaj <ermal.bj@gmail.com>
2023-04-10 20:24:55 +0200

    Initial commit

---
+++ Financify/FinancifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancifyApp.swift
+//  Financify
+//
+//  Created by Ermal Bujupaj on 10.04.23.
+//
+
+import SwiftUI
+
+@main
+struct FinancifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
