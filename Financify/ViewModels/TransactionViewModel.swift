3fa4f0661980a4b3157c4c6ef4391ce930db8331
Ermal Bujupaj <ermal.bj@gmail.com>
2023-04-13 22:02:42 +0200

    WIP: add TransactionViewModel

---
+++ Financify/ViewModels/TransactionViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionViewModel.swift
+//  Financify
+//
+//  Created by Ermal Bujupaj on 13.04.23.
+//
+
+import SwiftUI
+import Combine
+
+class TransactionViewModel: ObservableObject {
+    
+    @Published var transactions: [Transaction] = []
+    @Published var filteredTransactions: [Transaction] = []
+    
+    private var cancellables = Set<AnyCancellable>()
+}
