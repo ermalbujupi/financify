52c85dea9b404707c0eb340be769608cce7ad65e
Ermal Bujupaj <ermal.bj@gmail.com>
2023-04-15 13:59:40 +0200

    Add function to fetch transactions in viewmodel

--- Financify/ViewModels/TransactionViewModel.swift
+++ Financify/ViewModels/TransactionViewModel.swift
@@ -12,14 +12,35 @@ class TransactionViewModel: ObservableObject {
     
     @Published var transactions: [Transaction] = []
     @Published var filteredTransactions: [Transaction] = []
+
+    @Published var isLoading = false
+    @Published var error: Error? = nil
     
+    private var databaseService: FirebaseService
     private var cancellables = Set<AnyCancellable>()
     
-    init() {
+    init(databaseService: FirebaseService = FirebaseService.shared) {
+        self.databaseService = databaseService
         loadTransactions()
     }
     
     private func loadTransactions() {
-        
+        isLoading = true
+        FirebaseService.shared.fetchTransactions()
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] completion in
+                guard let self = self else { return }
+                self.isLoading = false
+                switch completion {
+                case .failure(let error):
+                    print(error.localizedDescription)
+                case .finished:
+                    break
+                }
+            } receiveValue: { transactions in
+                self.transactions = transactions
+                self.filteredTransactions = transactions
+            }
+            .store(in: &cancellables)
     }
 }
