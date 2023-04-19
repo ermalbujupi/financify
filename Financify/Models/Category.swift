660ce1cc329ba02f54b6a5cee6707264ed54aff9
Ermal Bujupaj <ermal.bj@gmail.com>
2023-04-19 18:28:33 +0200

    Make Category model Hashable

--- Financify/Models/Category.swift
+++ Financify/Models/Category.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Category: Identifiable, Codable {
+struct Category: Identifiable, Codable, Hashable {
     var id: String
     var name: String
     var type: TransactionType
